33feb5392e95a9153408557b0a4d3a062c5049ad
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-05 20:31:25 +0200

    chore: refactor action name and improve response formatting in worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -19,39 +19,37 @@ export default {
     }
 
     const actions = {
-      getFullPalette: async () => {
+      'get-full-palette': async () => {
         try {
-          const body = request.body ? await request.json() : null;
+          const body = request.body ? await request.json() : null
           const data = new Data({
             base: body.base,
             themes: body.themes,
-          }).makePaletteData(
-            body.previous !== undefined ? body.previous : undefined
-          );
+          }).makePaletteData(body.previous !== undefined ? body.previous : undefined)
 
           if (data === null || data === undefined) {
             return new Response(
               JSON.stringify({
-                message: "The provided palette is not valid",
+                message: 'The provided palette is not valid',
               }) as BodyInit,
               {
                 status: 400,
                 headers: {
-                  "Access-Control-Allow-Origin": "*",
+                  'Access-Control-Allow-Origin': '*',
                 },
-              }
-            );
+              },
+            )
           } else {
             return new Response(JSON.stringify(data) as BodyInit, {
               status: 200,
               headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
               },
-            });
+            })
           }
         } catch (err) {
-          console.error(`KV returned error: ${err}`);
+          console.error(`KV returned error: ${err}`)
           return new Response(
             JSON.stringify({
               message: err,
@@ -59,13 +57,13 @@ export default {
             {
               status: 500,
               headers: {
-                "Access-Control-Allow-Origin": "*",
+                'Access-Control-Allow-Origin': '*',
               },
-            }
-          );
+            },
+          )
         }
       },
-    };
+    }
 
     if (endpoint && actions[endpoint]) {
       return actions[endpoint]();
